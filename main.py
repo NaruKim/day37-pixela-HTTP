8754153aeeb7306f39e3341f8ce0ce421637c395
Naru <58687191+NaruKim@users.noreply.github.com>
2021-03-06 00:35:21 +0900

    Add files via upload

--- main.py
+++ main.py
@@ -2,11 +2,14 @@ import requests
 import datetime
 
 now = datetime.datetime.now().date()
-
+TODAY = now.strftime('%Y%m%d') # C언어가 떠오르는 기법이다.
+# today = datetime(year=2021, month=3, day=6)
+# today.strftime('%Y%m%d')
+CHANGE_DAY = 0 #yyyymmdd
 
 USERNAME = "naru"
 TOKEN = "black5598"
-GRAPHID = "graph1"
+GRAPH_ID = "graph1"
 
 HEADERS = {
     "X-USER-TOKEN": TOKEN
@@ -36,14 +39,34 @@ graph_config = {
     "color": "kuro",
 }
 
-
 # response = requests.post(url=graph_endpoint, json=graph_config, headers=HEADERS)
 # print(response.text)
 
-# value_endpoint = f"{pixela_endpoint}/{USERNAME}/graphs/{GRAPHID}"
-#
-# value_config = {
-#     "date": ,
-#     "quantity":,
-# }
+
+value_endpoint = f"{pixela_endpoint}/{USERNAME}/graphs/{GRAPH_ID}"
+
+value_quantity = "20"
+value_config = {
+    "date": TODAY,
+    "quantity": input("KM : "),
+}
+
+# response = requests.post(url=value_endpoint, json=value_config, headers=HEADERS)
+# print(response.text)
+
+
+put_endpoint = f"{pixela_endpoint}/{USERNAME}/graphs/{GRAPH_ID}/{CHANGE_DAY}"
+
+put_config = {
+    "quantity": "15"
+}
+
+# response = requests.put(url=put_endpoint, json=put_config, headers=HEADERS)
+# print(response.text)
+
+
+delete_endpoint = f"{pixela_endpoint}/{USERNAME}/graphs/{GRAPH_ID}/{CHANGE_DAY}"
+
+# response = requests.delete(url=delete_endpoint, headers=HEADERS)
+# print(response.text)
 
